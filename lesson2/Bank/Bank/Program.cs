157a01f59a9d00ae006c824a5657295472047cca
evgmolt <evg.molt@gmail.com>
2021-12-22 11:17:35 +0400

    Bank account class completed

---
+++ lesson2/Bank/Bank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account();
+            Account account2 = new Account(100);
+            Account account3 = new Account(200, AccountType.Brokerage);
+
+            Console.WriteLine(account1);
+            Console.WriteLine(account2);
+            Console.WriteLine(account3);
+
+            account3.Deposit(200);
+            Console.WriteLine(account3.Withdraw(50));
+            Console.WriteLine(account3);
+            Console.WriteLine(account3.Withdraw(1000));
+            Console.WriteLine(account3);
+            Console.ReadKey();
+        }
+    }
+}
