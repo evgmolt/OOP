2d14ef9165fd237ede616303d46c4832f5af0bbd
evgmolt <evg.molt@gmail.com>
2021-12-25 09:29:37 +0400

    lesson2, bank - Transfer method added, lesson3 - strings utils completed

--- lesson2/Bank/Bank/Program.cs
+++ lesson2/Bank/Bank/Program.cs
@@ -14,15 +14,20 @@ namespace Bank
             Account account2 = new Account(100);
             Account account3 = new Account(200, AccountType.Brokerage);
 
-            Console.WriteLine(account1);
-            Console.WriteLine(account2);
-            Console.WriteLine(account3);
+            Console.WriteLine("1 " + account1);
+            Console.WriteLine("2 " + account2);
+            Console.WriteLine("3 " + account3);
 
+            Console.WriteLine("3 deposit 200"); 
             account3.Deposit(200);
-            Console.WriteLine(account3.Withdraw(50));
+            Console.WriteLine("3 Withdraw 50 --> " + account3.Withdraw(50));
             Console.WriteLine(account3);
-            Console.WriteLine(account3.Withdraw(1000));
+            Console.WriteLine("3 Withdraw 1000 --> " + account3.Withdraw(1000));
             Console.WriteLine(account3);
+            Console.WriteLine("Transfer from 3 to 2 30 --> " + account2.Transfer(account3, 30));
+            Console.WriteLine("Transfer from 3 to 2 3000 --> " + account2.Transfer(account3, 3000));
+            Console.WriteLine("2 " + account2);
+            Console.WriteLine("3 " + account3);
             Console.ReadKey();
         }
     }
