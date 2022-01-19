a8dbc75faf4deb30e21188d0b044bc86a7212d1e
evgmolt <evg.molt@gmail.com>
2022-01-19 13:31:06 +0400

    Coder and MyList completed

---
+++ lesson7/CoderDemo/ICoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderDemo
+{
+    internal interface ICoder
+    {
+        string Encode(string context);
+        string Decode(string context);
+    }
+}
